2145814ebc1b59fd80edadaa93a20a1b21ac6a45
Javier Provecho Fernandez <javiertitan@gmail.com>
2015-10-24 13:08:38 +0200

    refactor + color y posicion

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ var unirest = require('unirest');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
+app.set('googleapikey', (process.env.GOOGLE));
 
 app.get('/raw', function(req, res) {
   unirest.get('http://hackathon.ttcloud.net:10026/v1/contextEntities/UOE9AW')
@@ -19,16 +20,34 @@ app.get('/all', function(req, res) {
     .header('Accept', 'application/json')
     .header('Fiware-Service', 'todosincluidos')
     .header('Fiware-ServicePath', '/iot')
-    .end(function(response){
-      var luminance = response.body.contextElement.attributes[8];
-      var humidity = response.body.contextElement.attributes[6];
-      var temperature = response.body.contextElement.attributes[16];
-      model = {
-        'luminance': luminance.value,
-        'humidity': humidity.value,
-        'temperature': temperature.value
-      };
-      res.send(model);
+    .end(function(responseA){
+      var data = responseA.body.contextElement;
+
+      unirest.post('https://www.googleapis.com/geolocation/v1/geolocate')
+        .query('key=' + app.get('googleapikey'))
+        .type('json')
+        .send({
+          'cellTowers': [
+            {
+              'cellId': parseInt(data.attributes[0].value, 16),
+              'locationAreaCode': parseInt(data.attributes[7].value, 16),
+              'mobileCountryCode': data.attributes[9].value,
+              'mobileNetworkCode': data.attributes[11].value,
+            }
+          ]
+        })
+        .end(function(responseB){
+          model = {
+            'luminance': data.attributes[8].value,
+            'humidity': data.attributes[6].value,
+            'temperature': data.attributes[16].value,
+            'color': data.attributes[3].value,
+            'latitude': responseB.body.location.lat,
+            'longitude': responseB.body.location.lng,
+            'accuracy': responseB.body.accuracy
+          };
+          res.send(model);
+        })
     });
 });
 
