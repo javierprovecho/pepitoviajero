d574c08b5eda2ddbc61a3b79f0eda6053c608d6a
Javier Provecho Fernandez <javiertitan@gmail.com>
2015-10-24 11:35:10 +0200

    anadido metodo de temperatura

--- main.js
+++ main.js
@@ -2,18 +2,30 @@ var express = require('express');
 var unirest = require('unirest');
 var app = express();
 
-var getData = function() {
+
+app.get('/raw', function(req, res) {
   unirest.get('http://hackathon.ttcloud.net:10026/v1/contextEntities/UOE9AW')
     .header('Accept', 'application/json')
     .header('Fiware-Service', 'todosincluidos')
     .header('Fiware-ServicePath', '/iot')
     .end(function(response){
-      return response
-  });
-  return null;
-}
-app.get('/all', function (req, res) {
-  res.send(getData());
+      res.send(response);
+    });
+});
+
+app.get('/temperature', function(req, res) {
+  unirest.get('http://hackathon.ttcloud.net:10026/v1/contextEntities/UOE9AW')
+    .header('Accept', 'application/json')
+    .header('Fiware-Service', 'todosincluidos')
+    .header('Fiware-ServicePath', '/iot')
+    .end(function(response){
+      var temperature = response.body.contextElement.attributes[16];
+      console.log(temperature);
+      model = {
+        'temperature': temperature.value
+      };
+      res.send(model);
+    });
 });
 
 var server = app.listen(3000, function () {
