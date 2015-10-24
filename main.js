07facc0dd639ea89434ae5f95acce84cf304fe66
Javier Provecho Fernandez <javiertitan@gmail.com>
2015-10-24 13:34:09 +0200

    force types

--- main.js
+++ main.js
@@ -5,16 +5,6 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 app.set('googleapikey', (process.env.GOOGLE));
 
-app.get('/raw', function(req, res) {
-  unirest.get('http://hackathon.ttcloud.net:10026/v1/contextEntities/UOE9AW')
-    .header('Accept', 'application/json')
-    .header('Fiware-Service', 'todosincluidos')
-    .header('Fiware-ServicePath', '/iot')
-    .end(function(response){
-      res.send(response);
-    });
-});
-
 app.get('/all', function(req, res) {
   unirest.get('http://hackathon.ttcloud.net:10026/v1/contextEntities/UOE9AW')
     .header('Accept', 'application/json')
@@ -38,13 +28,13 @@ app.get('/all', function(req, res) {
         })
         .end(function(responseB){
           model = {
-            'luminance': data.attributes[8].value,
-            'humidity': data.attributes[6].value,
-            'temperature': data.attributes[16].value,
+            'luminance': parseFloat(data.attributes[8].value),
+            'humidity': parseInt(data.attributes[6].value),
+            'temperature': parseFloat(data.attributes[16].value),
             'color': data.attributes[3].value,
-            'latitude': responseB.body.location.lat,
-            'longitude': responseB.body.location.lng,
-            'accuracy': responseB.body.accuracy
+            'latitude': parseFloat(responseB.body.location.lat),
+            'longitude': parseFloat(responseB.body.location.lng),
+            'accuracy': parseInt(responseB.body.accuracy)
           };
           res.send(model);
         })
@@ -52,10 +42,6 @@ app.get('/all', function(req, res) {
 });
 
 var server = app.listen(app.get('port'), function () {
-
-  var host = server.address().address;
   var port = server.address().port;
-
-  console.log('Example app listening at http://%s:%s', host, port);
-
+  console.log('Pepito Viajero escuchando en el puerto %s', server.address().port);
 });
