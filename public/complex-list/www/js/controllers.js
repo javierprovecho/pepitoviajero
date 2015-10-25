73e07f62fad937189ef0383799048f1ffdb0cd3b
Javier Provecho Fernandez <javiertitan@gmail.com>
2015-10-25 12:29:51 +0100

    anadido markers, circulo de precision e icono

--- public/complex-list/www/js/controllers.js
+++ public/complex-list/www/js/controllers.js
@@ -3,6 +3,22 @@ angular.module('starter.controllers', ['ionic'])
 .controller('MapCtrl', function($scope, $ionicLoading) {
   $scope.mapCreated = function(map) {
     $scope.map = map;
+    $scope.icon = 'http://pepitoviajero.herokuapp.com/img/pepito.png';
+    $scope.marker = new google.maps.Marker({
+      position: new google.maps.LatLng(43.07493, -89.381388),
+      map: $scope.map,
+      icon: $scope.icon
+    });
+    $scope.circle = new google.maps.Circle({
+        strokeColor: '#FF0000',
+        strokeOpacity: 0.8,
+        strokeWeight: 2,
+        fillColor: '#FF0000',
+        fillOpacity: 0.35,
+        map: $scope.map,
+        center: new google.maps.LatLng(43.07493, -89.381388),
+        radius: 600
+    });
   };
 
   $scope.centerOnMe = function () {
